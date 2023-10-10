24bc1a06e11b14aa3c5ba12a43741c784980d19c
Noodleman9570 <kevinsaavedra55@gmail.com>
2023-10-10 06:54:57 -0400

    importacion de tailwindcss por cdn, arreglar la importacion por npm

--- resources/views/cursos/index.blade.php
+++ resources/views/cursos/index.blade.php
@@ -13,4 +13,6 @@
         @endforeach
     </ul>
 
+    {{ $cursos->links() }}
+
 @endsection
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -1,17 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- @vite('resources/css/app.css') --}}
+    <script src="https://cdn.tailwindcss.com"></script>
     <title>@yield('title')</title>
     <!-- favicon -->
     <!-- estilos -->
 </head>
+
 <body>
     <!-- header -->
     <!-- nav -->
-   @yield('content')
+    @yield('content')
     <!-- footer -->
     <!-- script -->
 </body>
+
 </html>
